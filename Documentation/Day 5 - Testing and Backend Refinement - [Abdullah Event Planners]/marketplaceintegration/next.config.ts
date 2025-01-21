f7ece2eece04a81d0690924218cd9b1fa6d78dd2
SaiyedMuhammadAnasMaududi <tafs4920@outlook.com>
2025-01-21 09:18:10 +0500

    DAY-5 Completed Alhamdulillah

---
+++ Documentation/Day 5 - Testing and Backend Refinement - [Abdullah Event Planners]/marketplaceintegration/next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["www.shutterstock.com","d1csarkz8obe9u.cloudfront.net"], // Allow images from Shutterstock
+  },
+};
+
+export default nextConfig;
